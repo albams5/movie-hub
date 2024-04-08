e779eb860f183e517c15239ae55aae48ada58a2b
Alba Martin <albams51289@gmail.com>
2024-04-08 19:26:05 +0200

    Movie and Genre controllers edited

--- src/controllers/genre.controllers.ts
+++ src/controllers/genre.controllers.ts
@@ -1,49 +1,102 @@
-import { Request, Response } from "express"
-import prisma from "../db/client"
+import { Request, Response } from "express";
+import prisma from "../db/client";
 
-export const getAllGenres = async(req:Request, res: Response) => {
-    try{
-        const allGenres = await prisma.genre.findMany({})
-        res.status(200).send(allGenres)
-    }catch(error){
-        res.status(400).send(error)
-    }
-}
-export const createGenre = async(req:Request, res: Response) => {
-    const {name} = req.body
-    try{
-        const newGenre = await prisma.genre.create({
-            data: {name}
-        })
-        res.status(201).send(newGenre)
-    }catch(error){
-        res.status(400).send(error)
-    }
-}
+export const getAllGenres = async (req: Request, res: Response) => {
+  try {
+    const allGenres = await prisma.genre.findMany({});
+    res.status(200).send({
+      msg: "All genres",
+      data: allGenres,
+      type: typeof allGenres,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+export const createGenre = async (req: Request, res: Response) => {
+  const { name } = req.body;
 
-export const updateGenre = async(req:Request, res: Response) => {
-    const {name} = req.body
-    const {genreID} = req.params
-    try{
-        const genreUpdated = await prisma.genre.update({
-            where: {id:genreID},
-            data: {name}
-        })
-        res.status(201).send(genreUpdated)
+  if (!name) {
+    return res.status(400).send({ message: "The field name is required" });
+  }
 
-    }catch(error){
-        res.status(400).send(error)
-    }
-}
+  try {
+    const newGenre = await prisma.genre.create({
+      data: { name },
+    });
+    res.status(201).send({
+      msg: "Genre created successfully",
+      data: newGenre,
+      type: typeof newGenre,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-export const deleteGenre = async(req:Request, res: Response) => {
-    const {genreID} = req.params
-    try{
-        const genreDeleted = await prisma.genre.delete({
-            where: {id:genreID}
-        })
-        res.status(201).send(genreDeleted)
-    }catch(error){
-        res.status(400).send(error)
+export const updateGenre = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { genreID } = req.params;
+
+  if (!name) {
+    return res.status(400).send({ message: "THe field name is required" });
+  }
+
+  try {
+    const genreUpdated = await prisma.genre.update({
+      where: { id: genreID },
+      data: { name },
+    });
+    res.status(201).send({
+      msg: "Genre updated successfully",
+      data: genreUpdated,
+      type: typeof genreUpdated,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deleteGenre = async (req: Request, res: Response) => {
+  const { genreID } = req.params;
+
+  if (!genreID) {
+    return res.status(400).send({
+      message: "The field genreID is required",
+    });
+  }
+
+  try {
+    const genre = await prisma.genre.findUnique({
+      where: {
+        id: genreID,
+      },
+    });
+    if (!genre) {
+      return res.status(404).send({
+        message: "Genre not found",
+      });
     }
-}
+
+    const deletedGenre = await prisma.$transaction(async (prisma) => {
+      await prisma.movieGenre.deleteMany({
+        where: {
+          genreID: genreID,
+        },
+      });
+      return prisma.genre.findUnique({
+        where: {
+          id: genreID,
+        },
+      });
+    });
+
+    res.status(201).send({
+        message: "Genre deleted successfully",
+        data: deletedGenre,
+        type: typeof deletedGenre
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
--- src/controllers/movie.controllers.ts
+++ src/controllers/movie.controllers.ts
@@ -4,11 +4,15 @@ import prisma from "../db/client";
 export const getAllMovies = async (req: Request, res: Response) => {
   try {
     const allMovies = await prisma.movie.findMany({
-      include:{
-        genre: true
-      }
+      include: {
+        genre: true,
+      },
     });
-    res.status(201).send(allMovies);
+    res.status(201).send({
+      msg: "All movies",
+      data: allMovies,
+      type: typeof allMovies
+    });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -16,40 +20,179 @@ export const getAllMovies = async (req: Request, res: Response) => {
 
 export const createMovie = async (req: Request, res: Response) => {
   const { name, image, score, genre } = req.body;
-  const {userID} = req.params
-  try {
-    const newMovie = await prisma.movie.create({
-      data:{ name, image, score, genre, user: {connect: {id:userID}} }
+  const { userID } = req.params;
+
+  if (!name || !image || !score || !genre) {
+    return res.status(400).send({
+      message: "The fields name, image, score and genre are required",
     });
-    res.status(201).send(newMovie);
+  }
+
+  if (!userID) {
+    return res.status(400).send({
+      message: "The field userID is required",
+    });
+  }
+
+  try {
+    const newMovie = await prisma.$transaction(async (prisma) => {
+      const movie = await prisma.movie.create({
+        data: { name, image, score, genre, userID },
+      });
+      if (genre && genre.length > 0) {
+        const createGenre = genre.map((genreID: number) => ({
+          movieID: movie.id,
+          genreID: genre.id,
+        }));
+        await prisma.movieGenre.createMany({
+          data: createGenre,
+        });
+      }
+      return prisma.movie.findUnique({
+        where: {
+          id: movie.id,
+        },
+        include: {
+          genre: true,
+        },
+      });
+    });
+    res.status(201).send({
+      msg: "Movie created successfully",
+      data: newMovie,
+      type: typeof newMovie,
+    });
   } catch (error) {
     res.status(400).send(error);
   }
 };
 
-export const updateMovie = async(req: Request, res: Response) => {
-  const {name, image, score, genre} = req.body
-  const {movieID} = req.params
-  try{
-    const movieUpdated = await prisma.movie.update({
-      where: {id:movieID},
-      data:{name, image, score, genre}
-    })
-    res.status(201).send(movieUpdated)
+export const updateMovie = async (req: Request, res: Response) => {
+  const { name, image, score, genre } = req.body;
+  const { movieID } = req.params;
 
-  }catch(error){
-    res.status(400).send(error)
+  if (!movieID) {
+    return res.status(400).send({
+      message: "The field movieID is required",
+    });
+  }
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieID,
+      },
+      include: {
+        genre: true,
+      },
+    });
+
+    if (!movie) {
+      return res.status(404).send({
+        message: "Movie not found",
+      });
+    }
+
+    const updatedMovie = await prisma.$transaction(async (prisma) => {
+      const updatedMovie = await prisma.movie.update({
+        where: {
+          id: movieID,
+        },
+        data: {
+          name: name,
+          image: image,
+          score: score,
+        },
+      });
+
+      if (genre && genre.length > 0) {
+        await prisma.movieGenre.deleteMany({
+          where: {
+            movieID: movieID,
+          },
+        });
+
+        const createGenre = genre.map((genreID: number) => ({
+          movieID: movieID,
+          genreID: genreID,
+        }));
+
+        await prisma.movieGenre.createMany({
+          data: createGenre,
+        });
+      }
+      return prisma.movie.findUnique({
+        where: {
+          id: movieID,
+        },
+        include: {
+          genre: true,
+        },
+      });
+    });
+    res.status(200).send({
+      message: "Movie updated successfully",
+      data: updatedMovie,
+      type: typeof updatedMovie,
+    });
+  } catch (error) {
+    res.status(400).send(error);
   }
 };
 
-export const deleteMovie = async(req: Request, res: Response) => {
-  const {movieID} = req.params
-    try{
-        const movieDeleted = await prisma.movie.delete({
-          where:{id:movieID}
-        })
-        res.status(201).send(movieDeleted)
-    }catch(error){
-        res.status(400).send(error)
+export const deleteMovie = async (req: Request, res: Response) => {
+  const { movieID } = req.params;
+
+  if (!movieID) {
+    return res.status(400).send({
+      message: "The field movieID is required",
+    });
+  }
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieID,
+      },
+      include: {
+        genre: true,
+      },
+    });
+
+    if (!movie) {
+      return res.status(400).send({
+        message: "Movie not found",
+      });
     }
+
+    const deletedMovie = await prisma.$transaction(async (prisma) => {
+      await prisma.movieGenre.deleteMany({
+        where: {
+          movieID: movieID,
+        },
+      });
+
+      const deletedMovie = await prisma.movie.delete({
+        where: {
+          id: movieID,
+        },
+      });
+
+      return prisma.movie.findUnique({
+        where: {
+          id: movieID,
+        },
+        include: {
+          genre: true,
+        },
+      });
+    });
+    res.status(201).send({
+      message: "Movie deleted successfully",
+      data: deletedMovie,
+      type: typeof deletedMovie,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
