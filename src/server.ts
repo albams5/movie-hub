c02903a55d1695a929e34059efd89cef6021723d
albams5 <albams51289@gmail.com>
2024-07-01 13:52:07 +0200

    clean: unused code deleted

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,9 @@ import express, { Express } from 'express'
 import userRoutes from './routes/user.routes';
 import movieRoutes from './routes/movie.routes';
 import genreRoutes from './routes/genre.routes';
-import { checkJwtMiddleware } from './middlewares/checkJwt_middleware';
 import cors from 'cors'
 import errorHandler from './middlewares/error.middleware';
 import { requestRouter } from './routes/requests.routes';
-// import fileUpload from 'express-fileupload'
 import { json, urlencoded } from 'body-parser';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -17,15 +15,12 @@ const app: Express = express();
 
 
 app.use(helmet())
-// app.use(express.json())
 app.use(json())
 app.use(urlencoded({ extended: true }));
 
 app.use(morgan('tiny'))
 
 app.use(cors({
-    // origin: 'http://localhost:3000',
-    // credentials: true
   }));
 
 
@@ -36,10 +31,6 @@ app.use(errorHandler)
 
 app.use("/public", requestRouter)
 
-// app.use(fileUpload({
-//     useTempFiles: true,
-//     tempFileDir: './uploads'
-// }))
 
 export default app
 
